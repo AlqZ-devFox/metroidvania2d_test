80ae840e437aa5bc34905814ebd9e325c261477e
amarcaz0808 <98592893+AlqZ-devFox@users.noreply.github.com>
2025-09-18 19:10:24 +0200

    Tinkered PlayerInput

--- Assets/PlayerInput/PlayerInput.cs
+++ Assets/PlayerInput/PlayerInput.cs
@@ -52,7 +52,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": false
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
