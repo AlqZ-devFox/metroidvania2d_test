846e06ca31fc674c536dcb111ef2bc814fd7a499
amarcaz0808 <98592893+AlqZ-devFox@users.noreply.github.com>
2025-09-18 18:06:33 +0200

    Updated PlayerAttack.cs cuz I was oh so wrong
    I forgot about many of the good practices of programming
    What I intended to do here will change, and mostly be transported to another Script inside the beam itself
    Plus, this way I don't have to make this script stupidly complex when I want to introduce missiles :3

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -8,14 +8,7 @@ public class PlayerAttack : MonoBehaviour
     #region Global Variables
     
     [SerializeField] private GameObject beam;
-    private GameObject[] _beams; //array of beam prefab
     [SerializeField] private Transform beamOrigin; //from where the shots are going to come from
-    [SerializeField] private int maxBeam = 64;
-    [SerializeField] private float waitToDestroy = 6;
-
-    [SerializeField] [Range(0.1f, 1000f)] private float beamDamage;
-    [SerializeField] [Range(0.1f, 1000f)] private float beamSpeed;
-
     //private bool _isTriple; //Worry about this later to test Wide Beam
     
     #endregion
@@ -24,20 +17,8 @@ public class PlayerAttack : MonoBehaviour
 
     void Awake()
     {
-        for (int i = 0; i < maxBeam; i++)
-        {
-            _beams[i] = Instantiate(beam, beamOrigin);
-            _beams[i].SetActive(false);
-        }//EndOf FOR Loop
         
-        beam.SetActive(false);
     }//EndOf Unity method Awake
-
-    void Update()
-    {
-        //Beam Logic
-        BeamLogic();
-    }//EndOf Unity method Update
 
     #endregion
     
@@ -45,34 +26,8 @@ public class PlayerAttack : MonoBehaviour
     
     public void OnShoot(InputAction.CallbackContext context)
     {
-        for (int i = 0; i < _beams.Length; i++)
-        {
-            if (context.performed && !_beams[i].activeInHierarchy)
-            {
-                _beams[i].SetActive(true);
-                StartCoroutine(DeactivateBeam(_beams[i]));
-                
-                return;
-            }//EndOf IF
-        }//EndOf FOR Loop
+        //
     }//EndOf method OnShoot
-
-    private IEnumerator DeactivateBeam(GameObject beam)
-    {
-        yield return new WaitForSeconds(waitToDestroy);
-        beam.SetActive(false);
-    }
-
-    private void BeamLogic()
-    {
-        for (int i = 0; i < _beams.Length; i++)
-        {
-            if (_beams[i].activeInHierarchy)
-            {
-                _beams[i].GetComponent<Rigidbody>().linearVelocity = Vector3.forward * (beamSpeed * Time.deltaTime);
-            }//EndOf IF
-        }//EndOf FOR Loop
-    }//EndOf method BeamLogic
     
     #endregion
 }
